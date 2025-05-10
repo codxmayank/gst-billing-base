c3bbbb3064c15b9559279c92b9ebf7dff7da0422
codxmayank <44097941+codxmayank@users.noreply.github.com>
2025-05-10 13:40:00 +0530

    add routes authentication flow

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-// src/app.ts
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import authPlugin from './plugins/auth';
+// import authPlugin from './plugins/auth';
 import { authRoutes } from './routes/auth.routes';
 import { userRoutes } from './routes/user.routes';
 
@@ -14,12 +13,11 @@ export const buildApp = async () => {
     origin: '*',
   });
 
-  await app.register(authPlugin);
+  // await app.register(authPlugin);
 
   await app.register(authRoutes);
   await app.register(userRoutes, {
     prefix: '/api/v1/user',
-    onRequest: [app.authenticate]
   });
   app.get('/health', async () => ({ ok: true }));
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { getUserController } from '../controllers/user.controller';
+import { authenticate } from '../plugins/auth';
 
 export const userRoutes = async (app: FastifyInstance) => {
-  app.get('/:userId', getUserController);
+  app.get('/:userId', authenticate(getUserController));
 }
